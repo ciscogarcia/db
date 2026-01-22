e8d2f0cd1ed5016e0e656ba4a133e93278bfa1d5
Francisco Garcia <fgarc253@ford.com>
2026-01-21 20:30:39 -0500

    initial commit

---
+++ postgres/postgres.go
@@ -0,0 +1,55 @@
+package postgres
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var conn *pgxpool.Pool
+
+type DBConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Database string `json:"database"`
+}
+
+func (d *DBConfig) GetURL() string {
+	url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", d.Username, d.Password, d.Host, d.Port, d.Database)
+	return url
+}
+
+func GetDB() *pgxpool.Pool {
+	if conn != nil {
+		return conn
+	}
+	return InitDB()
+}
+
+func InitDB() *pgxpool.Pool {
+	// Get Credentials from config
+	file, err := os.Open("config.json")
+	if err != nil {
+		// log error
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	dbConfig := DBConfig{}
+	err = decoder.Decode(&dbConfig)
+	if err != nil {
+		// log error
+	}
+	// Get connection
+	conn, err = pgxpool.New(context.Background(), dbConfig.GetURL())
+	if err != nil {
+		// log err
+	}
+
+	// Return connection
+	return conn
+}
